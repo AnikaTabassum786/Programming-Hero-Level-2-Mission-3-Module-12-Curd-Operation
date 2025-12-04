8a82fc691c964aadd109b9d5784f146da8db3cb5
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-12-04 14:06:06 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,50 @@
+import express, { Request, Response } from "express"
+import { Pool } from 'pg'
+
+const app = express()
+const port = 5000
+
+//parser
+app.use(express.json())
+
+//DB
+
+const pool = new Pool({
+    connectionString: `postgresql://neondb_owner:npg_SGTFU6RIox7h@ep-misty-wildflower-a4e2x22w-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+});
+
+const initDB = async()=>{
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW(),
+        )
+        `)
+}
+
+initDB()
+
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello Anika Tabassum!')
+})
+
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body);
+
+    res.status(201).json({
+        success: true,
+        message: "API is working"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
