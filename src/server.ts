023a0217ffc84435f2d67124c2fac37f143d6a55
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-12-05 23:09:36 +0600

    middleware added

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Pool } from 'pg';
 import dotenv from "dotenv";
 import path from "path"
@@ -44,6 +44,16 @@ const initDB = async () => {
 }
 
 initDB()
+
+// logger middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+  next();
+};
+
+app.get("/", logger, (req: Request, res: Response) => {
+  res.send("Hello Next Level Developers!");
+});
 
 
 app.get('/', (req: Request, res: Response) => {
@@ -216,6 +226,14 @@ app.get('/todos', async (req: Request, res: Response) => {
         })
     }
 })
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path,
+  });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
