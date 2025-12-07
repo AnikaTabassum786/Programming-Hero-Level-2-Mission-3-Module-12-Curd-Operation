e3f2161687729f6c8823d042dcf8611ff60ec7bb
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-12-07 21:03:54 +0600

    JWT continoue

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { pool } from "../../config/db";
 import { userController } from "./user.controller";
+import auth from "../../middleware/auth";
 
 const router = express.Router();
 
@@ -9,9 +10,11 @@ const router = express.Router();
 // for this "/users" , this is root '/'
 
 router.post('/', userController.createUser)
-router.get('/', userController.getUser)
+router.get('/', auth(), userController.getUser)
 router.get('/:id', userController.getSingleUser)
 router.put('/:id', userController.updateUser)
 router.delete('/:id', userController.deleteUser)
+
+
 
 export const userRoutes = router;
